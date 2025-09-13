8de0d0ab42713161450f647d29b5af488f35c362
Franco Agustin Parra <fa.parra2001@gmail.com>
2025-09-12 23:17:52 -0300

    Crear clase Juego inicial con imports de Tablero, Jugadores y turnos

--- core/juego/juego.py
+++ core/juego/juego.py
@@ -1,2 +1,16 @@
-class Juego():
-    def __init__():
+from core.tablero.tablero import Tablero
+from core.jugador.jugador import Jugador
+from core.dados.dados import Dados
+
+class Juego:
+    def __init__(self, jugador1: Jugador, jugador2: Jugador):
+        self.tablero = Tablero()
+        self.jugadores = [jugador1, jugador2]
+        self.turno_idx = 0
+        self.dados = Dados()
+
+    def jugador_actual(self) -> Jugador:
+        return self.jugadores[self.turno_idx]
+
+    def cambiar_turno(self):
+        self.turno_idx = 1 - self.turno_idx
